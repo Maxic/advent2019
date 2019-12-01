5dfd33a8eefceeb76cfc0dbf3f7c539cfd1cdcf4
Barend <barend.poot@gmail.com>
2019-12-01 10:52:50 +0100

    day1, both problems

--- day1/problem2.py
+++ day1/problem2.py
@@ -5,11 +5,18 @@ def main():
         content = file.readlines()
         for line in content:
             mass = int(line)
-            fuel = mass // 3 - 2
-            fuel_sum += fuel
+            fuel_sum += calc_fuel(mass)
     print(fuel_sum)
+
+
+def calc_fuel(mass):
+    fuel = 0
+    while mass > 0:
+        mass = mass // 3 - 2
+        if mass > 0:
+            fuel += mass
+    return fuel
 
 
 if __name__ == "__main__":
     main()
-
